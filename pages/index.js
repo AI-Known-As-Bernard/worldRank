722c2cdcf3f2e65dfc58644cffc44a5893d9d310
AI-Known-As-Bernard <89490904+AI-Known-As-Bernard@users.noreply.github.com>
2022-04-21 21:11:06 -0700

    Finished
    Added Live search, validated countries to avoid renderer errors when no data is available for a field

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,54 @@
-import Head from "next/head";
-import { useState } from "react";
-import CountriesTable from "../components/CountriesTable/CountriesTable";
-import Layout from "../components/Layout/Layout";
-import SearchInput from "../components/SearchInput/SearchInput";
-import styles from "../styles/Home.module.css";
+import { useState } from 'react';
+import CountriesTable from '../components/CountriesTable/CountriesTable';
+import Layout from '../components/Layout/Layout';
+import SearchInput from '../components/SearchInput/SearchInput';
+import styles from '../styles/Home.module.css';
 
 export default function Home({ countries }) {
-  const [keyword, setKeyword] = useState("");
-  // console.log(countries)
-  countries.map((country)=>{
+	const [keyword, setKeyword] = useState('');
 
-  })
-  const filteredCountries = countries.filter(country => {
-      country.population > 0;
-    })
- 
-  
+	const filteredCountries = countries.filter((country) => {
+		if (country.name.toLowerCase().includes(keyword)) return country;
+		if (country.region.toLowerCase().includes(keyword))
+			return country;
+		if (country.subregion.toLowerCase().includes(keyword))
+			return country;
+	});
 
-  const onInputChange = (e) => {
-    e.preventDefault();
+	const onInputChange = (e) => {
+		e.preventDefault();
+		setKeyword(e.target.value.toLowerCase());
+	};
 
-    setKeyword(e.target.value.toLowerCase());
-    // filteredCountries = countries.filter(country => {
-    //   country.population > 5;
-    // })
-console.log(filteredCountries)
+	return (
+		<Layout>
+			<div className={styles.inputContainer}>
+				<div className={styles.counts}>
+					Found {countries.length} countries
+				</div>
 
-  };
+				<div className={styles.input}>
+					<SearchInput
+						placeholder='Filter by Name, Region or SubRegion'
+						onChange={onInputChange}
+					/>
+				</div>
+			</div>
 
-  return (
-    <Layout>
-      <div className={styles.inputContainer}>
-        <div className={styles.counts}>Found {countries.length} countries</div>
-
-        <div className={styles.input}>
-          <SearchInput
-            placeholder="Filter by Name, Region or SubRegion"
-            onChange={onInputChange}
-          />
-        </div>
-      </div>
-
-      <CountriesTable countries={countries} />
-    </Layout>
-  );
+			<CountriesTable countries={filteredCountries} />
+		</Layout>
+	);
 }
 
-export const getStaticProps = async () => {
-  const res = await fetch("https://restcountries.com/v2/all");
-  const countries = await res.json();
-  if(!countries.length){console.log("Not Working")}
-  return {
-    props: {
-      countries,
-    },
-  };
+export const getServerSideProps = async () => {
+	const res = await fetch('https://restcountries.com/v2/all');
+	const countries = await res.json();
+	if (!countries.length) {
+		console.log('Not Working');
+	}
+	return {
+		props: {
+			countries,
+		},
+	};
 };
